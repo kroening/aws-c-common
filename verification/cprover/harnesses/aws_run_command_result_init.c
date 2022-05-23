a79915525a1b89c0e3af8b150763d868418aada5
Daniel Kroening <dkr@amazon.com>
2022-05-23 16:29:29 +0100

    extend harness for aws_run_command_result_init

--- verification/cprover/harnesses/aws_run_command_result_init.c
+++ verification/cprover/harnesses/aws_run_command_result_init.c
@@ -9,8 +9,10 @@
 
 int main() {
 
-  struct aws_allocator *allocator;
+  struct aws_allocator *allocator = aws_default_allocator();
   struct aws_run_command_result *result;
+
+  __CPROVER_assume(__CPROVER_rw_ok(result));
 
   aws_run_command_result_init(allocator, result);
 
