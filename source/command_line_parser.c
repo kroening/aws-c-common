7aad768fcb5e21a2b12337601e0b7f7497949f5e
Daniel Kroening <dkr@amazon.com>
2022-05-24 19:08:55 +0100

    loop invariant for aws_cli_dispatch_on_subcommand

--- source/command_line_parser.c
+++ source/command_line_parser.c
@@ -145,7 +145,9 @@ int aws_cli_dispatch_on_subcommand(
     void *user_data) {
     if (argc >= 2) {
         struct aws_byte_cursor arg_name = aws_byte_cursor_from_c_str(argv[1]);
-        for (int i = 0; i < table_length; ++i) {
+        for (int i = 0; i < table_length; ++i)
+          __CPROVER_loop_invariant(__CPROVER_r_ok(dispatch_table, sizeof(*dispatch_table)*table_length))
+        {
             struct aws_byte_cursor cmd_name = aws_byte_cursor_from_c_str(dispatch_table[i].command_name);
 
             if (aws_byte_cursor_eq_ignore_case(&arg_name, &cmd_name)) {
