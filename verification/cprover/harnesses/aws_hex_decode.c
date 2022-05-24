74ab994e8b1a3ea9850d033dc57edbf4a750833c
Daniel Kroening <dkr@amazon.com>
2022-05-24 23:04:57 +0100

    progress on harness for aws_hex_decode

--- verification/cprover/harnesses/aws_hex_decode.c
+++ verification/cprover/harnesses/aws_hex_decode.c
@@ -3,6 +3,8 @@
 
 #include <aws/common/encoding.h>
 
+#include "aws_byte_buf.inc"
+
 // int aws_hex_decode(
 //     const struct aws_byte_cursor *AWS_RESTRICT to_decode,
 //     struct aws_byte_buf *AWS_RESTRICT output);
@@ -11,6 +13,9 @@ int main() {
 
   const struct aws_byte_cursor *to_decode;
   struct aws_byte_buf *output;
+
+  __CPROVER_assume(CPROVER_aws_byte_cursor_is_valid(to_decode));
+  __CPROVER_assume(CPROVER_aws_byte_buf_is_valid(output));
 
   aws_hex_decode(to_decode, output);
 
