da00b78198901694a8a036ed51e6a7c697ce8a81
Daniel Kroening <dkr@amazon.com>
2022-05-31 20:30:14 +0200

    extend harness

--- verification/cprover/harnesses/aws_array_list_clean_up_secure.c
+++ verification/cprover/harnesses/aws_array_list_clean_up_secure.c
@@ -5,9 +5,23 @@
 
 // void aws_array_list_clean_up_secure(struct aws_array_list *AWS_RESTRICT list);
 
+struct aws_array_list zero_list;
+
 int main() {
 
-  struct aws_array_list *AWS_RESTRICT list;
+  struct aws_array_list *list;
+
+  // the list can either be zeroed or have valid pointers
+  if(nondet_bool())
+  {
+    __CPROVER_assume(*list == zero_list);
+  }
+  else
+  {
+    __CPROVER_assume(list->alloc == aws_default_allocator());
+    __CPROVER_assume(__CPROVER_rw_ok(list->data, list->current_size));
+    // list->length * list->item_size < list->current_size
+  }
 
   aws_array_list_clean_up_secure(list);
 
