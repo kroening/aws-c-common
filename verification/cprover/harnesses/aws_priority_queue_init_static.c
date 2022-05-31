9ddcbbd14f5b3b5814cfdfd195b5daba50e20198
Daniel Kroening <dkr@amazon.com>
2022-05-31 20:39:07 +0200

    aws_priority_queue_init_static now works

--- verification/cprover/harnesses/aws_priority_queue_init_static.c
+++ verification/cprover/harnesses/aws_priority_queue_init_static.c
@@ -10,13 +10,22 @@
 //     size_t item_size,
 //     aws_priority_queue_compare_fn *pred);
 
+int nondet_int();
+
+int my_compare_fn(const void *a, const void *b)
+{
+  return nondet_int();
+}
+
 int main() {
 
   struct aws_priority_queue *queue;
   void *heap;
   size_t item_count;
   size_t item_size;
-  aws_priority_queue_compare_fn *pred;
+  aws_priority_queue_compare_fn *pred = my_compare_fn;
+
+  __CPROVER_assume(__CPROVER_rw_ok(queue));
 
   aws_priority_queue_init_static(queue, heap, item_count, item_size, pred);
 
