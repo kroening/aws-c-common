3bbb10b0f5ac581329f7b3b80da1666eec8dbaa8
Daniel Kroening <dkr@amazon.com>
2022-06-09 17:45:09 +0100

    harness for aws_base64_decode

--- verification/cprover/harnesses/aws_base64_decode.c
+++ verification/cprover/harnesses/aws_base64_decode.c
@@ -3,6 +3,8 @@
 
 #include <aws/common/encoding.h>
 
+#include "aws_byte_buf.inc"
+
 // int aws_base64_decode(
 //     const struct aws_byte_cursor *AWS_RESTRICT to_decode,
 //     struct aws_byte_buf *AWS_RESTRICT output);
@@ -11,6 +13,9 @@ int main() {
 
   const struct aws_byte_cursor *AWS_RESTRICT to_decode;
   struct aws_byte_buf *AWS_RESTRICT output;
+
+  __CPROVER_assume(CPROVER_aws_byte_cursor_is_valid(to_decode));
+  __CPROVER_assume(CPROVER_aws_byte_buf_is_valid(output));
 
   aws_base64_decode(to_decode, output);
 
