d34d6d77a4a958836e66dba2ffee1e935ae9a26f
Daniel Kroening <dkr@amazon.com>
2022-06-10 15:44:16 +0100

    fix harness for aws_byte_cursor_next_split

--- verification/cprover/harnesses/aws_byte_cursor_next_split.c
+++ verification/cprover/harnesses/aws_byte_cursor_next_split.c
@@ -10,16 +10,36 @@
 //     char split_on,
 //     struct aws_byte_cursor *AWS_RESTRICT substr);
 
+bool nondet_bool();
+
 int main() {
 
   const struct aws_byte_cursor *input_str;
   char split_on;
-  struct aws_byte_cursor *substr;
+  struct aws_byte_cursor substr;
 
   __CPROVER_assume(CPROVER_aws_byte_cursor_is_valid(input_str));
-  __CPROVER_assume(CPROVER_aws_byte_cursor_is_valid(substr));
 
-  aws_byte_cursor_next_split(input_str, split_on, substr);
+  // The function calls memchr on input->ptr unconditionally,
+  // which thus must be valid even when input->len is zero.
+  __CPROVER_assume(__CPROVER_r_ok(input_str->ptr, input_str->len));
+
+  // The cursor must be zeroed or point into the input_str
+  if(nondet_bool())
+  {
+    substr.ptr = 0;
+    substr.len = 0;
+  }
+  else
+  {
+    __CPROVER_size_t offset_start, offset_end;
+    __CPROVER_assume(offset_end <= input_str->len);
+    __CPROVER_assume(offset_start <= offset_end);
+    substr.ptr = input_str->ptr + offset_start;
+    substr.len = offset_end - offset_start;
+  }
+
+  aws_byte_cursor_next_split(input_str, split_on, &substr);
 
   return 0;
 }
