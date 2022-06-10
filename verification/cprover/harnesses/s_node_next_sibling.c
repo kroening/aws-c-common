230e888f1a1902e17ffddc105033a1bb23c33669
Daniel Kroening <dkr@amazon.com>
2022-06-10 14:45:20 +0100

    extend harness for s_node_next_sibling

--- verification/cprover/harnesses/s_node_next_sibling.c
+++ verification/cprover/harnesses/s_node_next_sibling.c
@@ -3,13 +3,19 @@
 
 #include <aws/common/xml_parser.h>
 
+#include <aws/common/private/xml_parser_impl.h>
+
+#include "aws_byte_buf.inc"
+
 int s_node_next_sibling(struct aws_xml_parser *parser);
 
 int main() {
 
-  struct aws_xml_parser *parser;
+  struct aws_xml_parser parser;
 
-  s_node_next_sibling(parser);
+  __CPROVER_assume(__CPROVER_r_ok(parser.doc.ptr, parser.doc.len));
+
+  s_node_next_sibling(&parser);
 
   return 0;
 }
